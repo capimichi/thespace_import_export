a065db94ad4d1921fb2ab4c3d04e412195a1cbcc
michele <capimichi@gmail.com>
2019-09-24 15:01:58 +0200

    Configurazione

---
+++ app/code/local/Thespace/ImportExport/controllers/ConfigurationController.php
@@ -0,0 +1,37 @@
+<?php
+
+class Thespace_ImportExport_ConfigurationController extends Mage_Adminhtml_Controller_Action
+{
+    
+    public function indexAction()
+    {
+        // "Fetch" display
+        $this->loadLayout();
+        
+        // "Inject" into display
+        // THe below example will not actualy show anything since the core/template is empty
+        $this->_addContent($this->getLayout()->createBlock('core/template')->setTemplate('thespace/import_export/import/configuration.phtml'));
+        
+        // "Output" display
+        $this->renderLayout();
+    }
+    
+    public function updateAction()
+    {
+        header('Content-Type: application/json');
+        $response = [
+            'status' => 'OK',
+            'errors' => [],
+        ];
+        
+        $importHelper = Mage::helper('thespaceimportexport/Import');
+        $csvHelper = Mage::helper('thespaceimportexport/Csv');
+        $productParserHelper = Mage::helper('thespaceimportexport/ProductParser');
+        
+        
+        
+        echo json_encode($response);
+        die();
+    }
+    
+}
